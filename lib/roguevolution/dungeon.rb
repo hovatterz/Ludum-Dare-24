11cf3761734b66c28dbb0f395a23b207094a941f
Zack Hovatter <zackhovatter@gmail.com>
2012-08-25 03:16:07 -0400

    Dungeon is now responsible for current floor

--- lib/roguevolution/dungeon.rb
+++ lib/roguevolution/dungeon.rb
@@ -5,6 +5,7 @@ module Roguevolution
     def initialize(num_floors, width, height)
       @width = width
       @height = height
+      @current_floor = 0
       @floors = [].fill(DungeonFloor.new(@width, @height), 0, num_floors)
     end
 
@@ -12,6 +13,14 @@ module Roguevolution
       @floors.each do |floor|
         floor.generate!
       end
+    end
+
+    def current_floor
+      @floors[@current_floor]
+    end
+
+    def tile_at(x, y)
+      @floors[@current_floor].tiles[[x, y]]
     end
   end
 end
--- lib/roguevolution/player.rb
+++ lib/roguevolution/player.rb
@@ -1,17 +1,16 @@
 module Roguevolution
   class Player
-    attr_reader :current_floor, :x, :y
+    attr_reader :x, :y
     
     def initialize
       @x = 0
       @y = 0
-      @current_floor = 0
     end
 
     def move(dungeon, x, y)
       pot_x = @x + x
       pot_y = @y + y
-      if dungeon.floors[@current_floor].passable?(pot_x, pot_y)
+      if dungeon.tile_at(pot_x, pot_y).passable?
         @x = pot_x
         @y = pot_y
       end
